80d8faf97b56ec05533da0ea586b30ce14f7c7b1
BananaPix <47648503+BananaPix4613@users.noreply.github.com>
2025-03-13 15:38:44 -0600

    Fixed dependencies and added GLM and got textures to render but transformations do not work for whatever reason.

--- src/SDLProject.cpp
+++ src/SDLProject.cpp
@@ -1,16 +1,39 @@
 ﻿#include <windows.h>
 #include <string>
-#include <fstream>
-#include <sstream>
+#include <iostream>
 #include <vector>
 #include <math.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <filesystem>
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include "shader.h"
 
 #define GLFW_INCLUDE_NONE
 #include <GLFW/glfw3.h>
-#include <glad/gl.h>
+#include <glad/glad.h>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+// Function declarations
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void GLAPIENTRY errorCallback(GLenum source, GLenum type, GLuint id,
+	GLenum severity, GLsizei length, const GLchar* message, const void* userParam);
+const char* getGLErrorString(GLenum error);
+
+bool initializeWindow(const char* title, int width, int height);
+bool initialize();
+void deinitialize();
+bool update();
+void processInput();
+void render(Shader &shader);
+void loop(Shader &shader);
+void setupTriangle(Shader &shader);
+
+// Global variables
 int* gFrameBuffer;
 int* gTempBuffer;
 GLFWwindow* gWindow;
@@ -18,64 +41,31 @@ static int gDone;
 const int WINDOW_WIDTH = 1920 / 2;
 const int WINDOW_HEIGHT = 1080 / 2;
 
-const char* vertexShaderSource = R"(
-#version 330 core
-layout(location = 0) in vec3 aPos;
-
-out vec4 vertexColor;
-
-void main()
-{
-	gl_Position = vec4(aPos, 1.0);
-	vertexColor = vec4(0.5, 0.0, 0.0, 1.0);
-}
-)";
-
-const char* fragmentShaderSource = R"(
-#version 330 core
-out vec4 FragColor;
-
-uniform vec4 ourColor;
-
-void main()
-{
-	FragColor = ourColor;
-}
-)";
-
 unsigned int shaderProgram;
 unsigned int VAO, VBO, EBO;
+unsigned int texture1, texture2;
 
 using namespace std;
 
-void GLAPIENTRY errorCallback(GLenum source, GLenum type, GLuint id,
-	GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+int main(int argc, char** argv)
 {
-	fprintf(stderr, "GL Error: type = 0x%x, severity = 0x%x, message = %s\n",
-		type, severity, message);
-}
-
-const char* getGLErrorString(GLenum error)
-{
-	switch (error)
+	if (!initialize())
 	{
-	case GL_NO_ERROR:
-		return "No error";
-	case GL_INVALID_ENUM:
-		return "Invalid enum";
-	case GL_INVALID_VALUE:
-		return "Invalid value";
-	case GL_STACK_OVERFLOW:
-		return "Stack overflow";
-	case GL_STACK_UNDERFLOW:
-		return "Stack underflow";
-	case GL_OUT_OF_MEMORY:
-		return "Out of memory";
-	case GL_INVALID_FRAMEBUFFER_OPERATION:
-		return "Invalid framebuffer operation";
-	default:
-		return "Unknown error";
+		return -1;
 	}
+
+	Shader shader("shaders/VertexShader.glsl", "shaders/FragmentShader.glsl");
+	setupTriangle(shader);
+
+	gDone = 0;
+	while (!gDone)
+	{
+		loop(shader);
+	}
+
+	deinitialize();
+
+	return 0;
 }
 
 bool initializeWindow(const char* title, int width, int height)
@@ -103,15 +93,16 @@ bool initializeWindow(const char* title, int width, int height)
 
 	// Create OpenGL context
 	glfwMakeContextCurrent(gWindow);
+	glfwSetFramebufferSizeCallback(gWindow, framebuffer_size_callback);
 
 	// Load OpenGL functions using GLAD
-	/*if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		fprintf(stderr, "Failed to initialize GLAD\n");
 		glfwDestroyWindow(gWindow);
 		glfwTerminate();
 		return false;
-	}*/
+	}
 
 	// Enable VSync (optional)
 	glfwSwapInterval(1);
@@ -168,7 +159,7 @@ bool initialize()
 	printf("GLSL Version: %s\n", glslVersion);
 
 	// Enable debug output
-	/*if (GLAD_GL_VERSION_4_3)
+	if (GLAD_GL_VERSION_4_3)
 	{
 		glEnable(GL_DEBUG_OUTPUT);
 		glDebugMessageCallback(errorCallback, 0);
@@ -176,19 +167,24 @@ bool initialize()
 	else
 	{
 		fprintf(stderr, "OpenGL 4.3 or higher is required for debug output\n");
-	}*/
+	}
 
 	return true;
 }
 
 void deinitialize()
 {
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteBuffers(1, &EBO);
+
 	glfwDestroyWindow(gWindow);
 	glfwTerminate();
 }
 
 bool update()
 {
+	processInput();
 	if (glfwWindowShouldClose(gWindow))
 	{
 		return false;
@@ -197,35 +193,52 @@ bool update()
 	return true;
 }
 
-void render(unsigned int aTicks)
+void processInput()
+{
+	if (glfwGetKey(gWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+	{
+		glfwSetWindowShouldClose(gWindow, true);
+	}
+}
+
+void render(Shader &shader)
 {
 	// Clear the screen with a green color
-	glClearColor(0.0f, 0.5f, 0.0f, 1.0f);
+	glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	float timeValue = glfwGetTime();
-	float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-	int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+	// Bind texture
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	glActiveTexture(GL_TEXTURE1);
+	glBindTexture(GL_TEXTURE_2D, texture2);
 
-	// Use the shader program
-	glUseProgram(shaderProgram);
-	glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+	shader.use();
 
-	// Bind the VA0
+	// Create transformations
+	glm::mat4 model = glm::mat4(1.0f);
+	glm::mat4 view = glm::mat4(1.0f);
+	glm::mat4 projection;
+	model = glm::rotate(model, glm::radians(-55.0f), glm::vec3(1.0f, 0.0f, 0.0f));
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	projection = glm::perspective(glm::radians(45.0f), (float)WINDOW_WIDTH / (float)WINDOW_HEIGHT, 0.1f, 100.0f);
+
+	unsigned int modelLoc = glGetUniformLocation(shader.ID, "model");
+	unsigned int viewLoc = glGetUniformLocation(shader.ID, "view");
+
+	glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+	glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
+	shader.setMat4("projection", projection);
+
+	// Render container
 	glBindVertexArray(VAO);
-
-	// Render the triangle
-	//glDrawArrays(GL_TRIANGLES, 0, 3);
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
-
-	// Unbind the VAO
-	glBindVertexArray(0);
 
 	glfwSwapBuffers(gWindow);
 	glfwPollEvents();
 }
 
-void loop()
+void loop(Shader &shader)
 {
 	if (!update())
 	{
@@ -233,22 +246,23 @@ void loop()
 	}
 	else
 	{
-		render(glfwGetTime());
+		render(shader);
 	}
 }
 
-void setupTriangle()
+void setupTriangle(Shader &shader)
 {
 	// Vertex data
 	float vertices[] = {
-			0.5f,  0.5f, 0.0f,  // Top right
-			0.5f, -0.5f, 0.0f,  // Bottom right
-		-0.5f, -0.5f, 0.0f,  // Bottom left
-		-0.5f,  0.5f, 0.0f   // Top left
+		// positions          // colors           // texture coords
+		 0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 1.0f,   1.0f, 1.0f,   // top right
+		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
+		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
+		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
 	};
 	unsigned int indices[] = {
-		0, 1, 3,  // First triangle
-		1, 2, 3   // Second triangle
+		0, 1, 3, // first triangle
+		1, 2, 3  // second triangle
 	};
 
 	// Create vertex array object and vertex buffer object
@@ -263,82 +277,105 @@ void setupTriangle()
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	// Bind EBO and upload indices data
+	// Bind EBO and upload vertex data
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-	// Set vertex attribute pointers
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	// Position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
-	// Unbind VBO and VAO
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-	//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
-	glBindVertexArray(0);
+	// Color attribute
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
 
-	// Compile vertex shader
-	unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-	glCompileShader(vertexShader);
+	// Texture coord attribute
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
+	glEnableVertexAttribArray(2);
 
-	// Check for vertex shader compile errors
-	int success;
-	char infoLog[512];
-	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-	if (!success)
+	// Load and create texture1
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	// Set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Load and generate the texture
+	int width, height, nrChannels;
+	stbi_set_flip_vertically_on_load(true);
+	unsigned char* data = stbi_load("assets/container.jpg", &width, &height, &nrChannels, 0);
+	if (data)
 	{
-		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-		fprintf(stderr, "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n%s\n", infoLog);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
 	}
-
-	// Compile fragment shader
-	unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-	glCompileShader(fragmentShader);
-
-	// Check for fragment shader compile errors
-	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-	if (!success)
+	else
 	{
-		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-		fprintf(stderr, "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n%s\n", infoLog);
+		std::cout << "Failed to load texture1" << std::endl;
 	}
+	stbi_image_free(data);
 
-	// Link shaders into a shader program
-	shaderProgram = glCreateProgram();
-	glAttachShader(shaderProgram, vertexShader);
-	glAttachShader(shaderProgram, fragmentShader);
-	glLinkProgram(shaderProgram);
-
-	// Check for linking errors
-	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-	if (!success)
+	// Load and create texture2
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	// Set the texture wrapping/filtering options (on the currently bound texture object)
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	// Load and generate the texture
+	data = stbi_load("assets/awesomeface.png", &width, &height, &nrChannels, 0);
+	if (data)
 	{
-		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-		fprintf(stderr, "ERROR::SHADER::PROGRAM::LINKING_FAILED\n%s\n", infoLog);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
 	}
+	else
+	{
+		std::cout << "Failed to load texture2" << std::endl;
+	}
+	stbi_image_free(data);
 
-	// Delete the shaders as they're linked into our program now and no longer necessary
-	glDeleteShader(vertexShader);
-	glDeleteShader(fragmentShader);
+	// Tell OpenGL for each sampler to which texture unit it belongs to (only has to be done once)
+	shader.use();
+	shader.setInt("texture1", 0);
+	shader.setInt("texture2", 1);
 }
 
-int main(int argc, char** argv)
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
-	if (!initialize())
+	// make sure the viewport matches the new window dimensions; note that width and
+	// height will be significantly larger than specified on retina displays
+	glViewport(0, 0, width, height);
+}
+
+void GLAPIENTRY errorCallback(GLenum source, GLenum type, GLuint id,
+	GLenum severity, GLsizei length, const GLchar* message, const void* userParam)
+{
+	fprintf(stderr, "GL Error: type = 0x%x, severity = 0x%x, message = %s\n",
+		type, severity, message);
+}
+
+const char* getGLErrorString(GLenum error)
+{
+	switch (error)
 	{
-		return -1;
+	case GL_NO_ERROR:
+		return "No error";
+	case GL_INVALID_ENUM:
+		return "Invalid enum";
+	case GL_INVALID_VALUE:
+		return "Invalid value";
+	case GL_STACK_OVERFLOW:
+		return "Stack overflow";
+	case GL_STACK_UNDERFLOW:
+		return "Stack underflow";
+	case GL_OUT_OF_MEMORY:
+		return "Out of memory";
+	case GL_INVALID_FRAMEBUFFER_OPERATION:
+		return "Invalid framebuffer operation";
+	default:
+		return "Unknown error";
 	}
-
-	setupTriangle();
-
-	gDone = 0;
-	while (!gDone)
-	{
-		loop();
-	}
-
-	deinitialize();
-
-	return 0;
 }
